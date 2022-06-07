4e95f8abb713c78235f7dd1b196da7fbf34f2f2f
Rohit <RohitSinghEmail@protonmail.com>
2022-06-07 16:40:39 +0530

    added stack.yaml

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -193,3 +193,5 @@ function notify
         functions -e _notify_job_$job
     end
 end
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/rohits/.ghcup/bin $PATH # ghcup-env
