4f8c576138c0ab597bebd5a0bf548fd083338fb2
Rohit <RohitSinghEmail@protonmail.com>
2023-04-15 14:40:03 +0530

    added nvim plugin files

---
+++ .config/nvim/plugin/airline.lua
@@ -0,0 +1,6 @@
+-- Vim Airline settings
+vim.g["airline#extensions#tabline#enabled"] = 1
+vim.g["airline#extensions#tabline#left_sep"] = ' '
+vim.g["airline#extensions#tabline#left_alt_sep"] = '|'
+vim.g["airline#extensions#tabline#formatter"] = 'unique_tail'
+
---
+++ .config/nvim/plugin/nvim_tree.lua
@@ -0,0 +1,20 @@
+-- NVIM setup with some options
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    width = 30,
+    mappings = {
+      list = {
+        { key = "u", action = "dir_up" },
+      },
+    },
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+
